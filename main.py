c1b0392f44a7aaa34349fe8c857c92a773899a67
Vladislav Zhdanovich <76701877+ZhdanovichVlad@users.noreply.github.com>
2022-05-07 19:43:06 +0600

    ver 0.16

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 # This is a sample Python script.
 import os
 import click
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import LinerRegression
 import get_dataset
 import mlflow
