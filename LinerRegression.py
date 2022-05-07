7864977dc73ecdd2561dcee035061814dc36440f
Vladislav Zhdanovich <76701877+ZhdanovichVlad@users.noreply.github.com>
2022-05-07 12:27:53 +0600

    ver 0.8

--- LinerRegression.py
+++ LinerRegression.py
@@ -16,15 +16,5 @@ def Liner_Model(X_train,y_train,random_state=42, n_cv = 5):
     model = pipline.create_pipeline(True,100,1.0,random_state)
     result = cross_validate(model, X_train, y_train, cv=n_cv, return_train_score=True,
                             scoring=('r2', 'neg_mean_squared_error', 'accuracy'), return_estimator=True)
-    print('test_r2', result['test_r2'])
-    print('test_neg_mean_squared_error', result['test_neg_mean_squared_error'])
-    print('test_balanced_accuracy', result['test_balanced_accuracy'])
-    # we will choose the model with the best test_balanced_accuracy
-    number_of_model = np.argmax(result['test_balanced_accuracy'])
-    answer = result['estimator'][number_of_model].predict(X_train)
+    number_of_model = np.argmax(result['test_accuracy'])
     return result['estimator'][number_of_model]
-    #balanced_accuracy = balanced_accuracy_score(y_test,answer)
-    #mse = result['test_neg_mean_squared_error'][number_of_model]
-    #print(r2)
-    #print(balanced_accuracy)
-    #print(mse)
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import pandas as pd
 import click
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import LinerRegression
 import get_dataset
 import mlflow
 from sklearn.ensemble import RandomForestClassifier
@@ -33,52 +34,40 @@ if __name__ == '__main__':
     else:
         print('you entered the wrong answer')
         exit()
+
     random_state = int(click.prompt('please enter random state (int) ', int))
     test_split_ratio = float(click.prompt('please enter test_split_ratio (float)',float))
     X_train, X_test, y_train, y_test = get_dataset.get_dataset(path,random_state,test_split_ratio)
 
-    n_cv_outer = int(click.prompt('please set the number of cv for outer cross validation', str))
-    n_cv_in = int(click.prompt('please set the number of cv for inner cross validation', str))
-    model_nsvwrf = nested_cross_validation_with_random_forest.nested_cv_rf(X_train,y_train,n_cv_outer,n_cv_in,
+    print('please select the model.')
+    print('Enter 1 if you want to select nested_cross_validation_with_random_forest')
+    print('Enter 2 if you want to select liner regression')
+    n_model = int(click.prompt('Enter the number', int))
+    if n_model == 1:
+        n_cv_outer = int(click.prompt('please set the number of cv for outer cross validation', int))
+        n_cv_in = int(click.prompt('please set the number of cv for inner cross validation', int))
+        model_nsvwrf = nested_cross_validation_with_random_forest.nested_cv_rf(X_train,y_train,n_cv_outer,n_cv_in,
                                                                            random_state)
+        model = model_nsvwrf['model']
+        model_name = 'nested_cross_validation_with_random_forest'
+
+    if n_model == 2:
+        n_cv_in = int(click.prompt('please set the number of cv for cross validation', int))
+        model = LinerRegression.Liner_Model(X_train,y_train,random_state,n_cv_in)
+        model_name = 'Liner Regression with cross validation'
+
 
     #mlflow.set_tracking_uri('http://localhost:5000')
 
     with mlflow.start_run():
-        model_nsvwrf = model_nsvwrf['model']
-        ansv_model_nsvwrf = model_nsvwrf.predict(X_test)
-        ansv_proba_model_nsvwrf = model_nsvwrf.predict_proba(X_test)
+        #model_nsvwrf = model_nsvwrf['model']
+        ansv_model_nsvwrf = model.predict(X_test)
+        ansv_proba_model_nsvwrf = model.predict_proba(X_test)
         acc = accuracy_score(y_test, ansv_model_nsvwrf)
         f1 = f1_score(y_test, ansv_model_nsvwrf, average='weighted')
         ras = roc_auc_score(y_test, ansv_proba_model_nsvwrf, multi_class='ovo')
-        mlflow.sklearn.log_model(model_nsvwrf, 'nested_cross_validation_with_random_forest')
+        mlflow.sklearn.log_model(model, model_name)
         mlflow.log_metric('accuracy',acc)
         mlflow.log_metric('f1', f1)
         mlflow.log_metric('roc_auc_score', ras)
-
-
-
-
-
-
-
-
-    #print(roc_auc_score(np.array(y_test), np.array(y_test), multi_class='ovo', average='weighted'))
-    #model = RandomForestClassifier(random_state=random_state)
-    # define search space
-    #space = dict()
-    #space['n_estimators'] = [10, 100, 500]
-    #space['max_features'] = [2, 4, 6]
-    # define search
-    #search = GridSearchCV(model, space, scoring=('r2','accuracy','balanced_accuracy'), n_jobs=1, cv=5, refit=True)
-    # configure the cross-validation procedure
-    #cv_outer = KFold(n_splits=10, shuffle=True, random_state=random_state)
-    # execute the nested cross-validation
-    #scores = cross_val_score(search, X_train, y_train, scoring='accuracy', cv=cv_outer, n_jobs=-1)
-    # report performance
-    #print('Accuracy: %.3f (%.3f)' % (np.mean(scores), np.std(scores)))
-
-
-
-    #print(mse)
-    #print(result)
+        click.echo(f"Model is saved.")
